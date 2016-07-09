7644c54844f638e0525ad8b8cc318f418e9105f5
snowfag <Yuki@lolicon.eu>
2016-07-09 02:19:15 -0700

    Added channel names to output.

--- youtube.py
+++ youtube.py
@@ -2,13 +2,13 @@ import requests
 import weechat as wc
 import re
 
-wc.register('youtube.py', 'snowfag', '1.0', 'BSD-2c', 'Youtube video title announcer', '', '')
+wc.register('youtube.py', 'snowfag', '1.1', 'BSD-2c', 'Youtube video title announcer', '', '')
 
 def config(*args, **kwargs):
     if not wc.config_is_set_plugin('channels'):
         wc.config_set_plugin('channels', '.*')
     if not wc.config_is_set_plugin('api_key'):
-	wc.config_set_plugin('api_key', 'not_set')
+        wc.config_set_plugin('api_key', 'not_set')
     if not wc.config_is_set_plugin('other_bots'):
         wc.config_set_plugin('other_bots', 'not_set')
     return wc.WEECHAT_RC_OK
@@ -30,17 +30,18 @@ def privmsg(data, signal, signal_data):
                 bots_test = wc.nicklist_search_nick(buffer_pointer, "", other_bots)
                 if bots_test:
                     bots_exist = True
-	if not bots_exist:
+        if not bots_exist:
             if not api_key == "not_set":
                 vid_id = youtube_regex_match.group(6)
                 rvt = requests.get('https://www.googleapis.com/youtube/v3/videos/?id={0}&part=snippet&key={1}'.format(vid_id, api_key))
                 rvc = requests.get('https://www.googleapis.com/youtube/v3/videos/?id={0}&part=statistics&key={1}'.format(vid_id, api_key))
                 try:
                     vid_title = rvt.json()['items'][0]['snippet']['title'].encode('utf-8')
+                    vid_channel = rvt.json()['items'][0]['snippet']['channelTitle'].encode('utf-8')
                     vid_views = rvc.json()['items'][0]['statistics']['viewCount']
                 except:
                     vid_title = "Error getting video info."
-                wc.command("", r"/msg {0} [Youtube] {1} | Views: {2}".format(buffer_name, vid_title, vid_views))
+                wc.command("", r"/msg {0} [Youtube] {1} | Channel: {2} | Views: {3}".format(buffer_name, vid_title, vid_channel, vid_views))
             else:
                 wc.command("", r"/msg {0} Youtube api key not set.".format(buffer_name))
     return wc.WEECHAT_RC_OK
